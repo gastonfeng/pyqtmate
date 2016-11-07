a0c6b693ab12e4213c2fbe7f63ef4a4fd677dd7a
冯建涛 <gastonfeng@qq.com>
2016-11-08 00:15:10 +0800

    QGroupBox删除子控件:子控件执行hide(),然后从布局removeWidget()。

--- buttonwithclose.py
+++ buttonwithclose.py
@@ -5,18 +5,21 @@ from guiqt import ui_compile
 
 ui_compile('buttonwc')
 from uibuttonwc import Ui_Form
-class buttonwc(QtGui.QFrame,Ui_Form):
-    Cilcked=pyqtSignal()
-    Closed=pyqtSignal()
+
+
+class buttonwc(QtGui.QFrame, Ui_Form):
+    Cilcked = pyqtSignal()
+    Closed = pyqtSignal()
+
     def __init__(self, *args):
         QtGui.QFrame.__init__(self, *args)
         self.setupUi(self)
 
-    def setText(self,txt):
+    def setText(self, txt):
         self.pushButton.setText(txt)
 
-    def on_pushButton_clicked(self,b):
+    def on_pushButton_clicked(self, b):
         self.Clicked()
 
-    def on_toolButton_clicked(self,b):
-        self.Closed()
+    def on_toolButton_clicked(self):
+        self.Closed.emit()
